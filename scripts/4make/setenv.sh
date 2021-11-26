5aa8db0d8883f171e7fcd8474eed521a270e95d7
Veren Damyanov <veren99@gmail.com>
2021-11-26 10:39:25 +0000

    Fix paths and symlink creation in install file #5

--- scripts/4make/setenv.sh
+++ scripts/4make/setenv.sh
@@ -3,7 +3,7 @@
 SRC_DIR="$(pwd)/src"
 
 cat << EOS
-export POSIXSH_STDLIB_HOME='$SRC_DIR'; \
+export POSIXSH_STDLIB_HOME="$SRC_DIR"; \
   export PATH="$SRC_DIR/bin:\$PATH"; \
-  export POSIXSH_IMPORT_PATH='$SRC_DIR:$SRC_DIR/unittest';
+  export POSIXSH_IMPORT_PATH="$SRC_DIR:$SRC_DIR/unittest";
 EOS
